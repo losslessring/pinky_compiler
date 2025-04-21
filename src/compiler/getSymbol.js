dbe9c5ec6824909477632ecb6cf1280211b90efb
Andrey <losslessring@yandex.ru>
2025-04-21 22:23:12 +0300

    Removed commented code from get symbol function

--- src/compiler/getSymbol.js
+++ src/compiler/getSymbol.js
@@ -8,24 +8,6 @@ export function getSymbol(compiler, name) {
     )
     assert(typeof name === 'string', `${name} is not of expected String type`)
 
-    // const localSymbol = compiler.locals.find((symbol) => symbol.name === name)
-    // if (localSymbol !== undefined) {
-    //     const localIndex = compiler.locals.findIndex(
-    //         (symbol) => symbol.name === name
-    //     )
-    //     return { symbol: localSymbol, index: localIndex }
-    // }
-
-    // const globalSymbol = compiler.globals.find((symbol) => symbol.name === name)
-    // if (localSymbol !== undefined) {
-    //     const globalIndex = compiler.globals.findIndex(
-    //         (symbol) => symbol.name === name
-    //     )
-
-    //     return { symbol: globalSymbol, index: globalIndex }
-    // }
-    // return undefined
-
     let localIndex = 0
     for (const localSymbol of compiler.locals) {
         if (localSymbol.name === name) {
